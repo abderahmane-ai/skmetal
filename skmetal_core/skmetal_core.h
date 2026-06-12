f318b44df68f5042e009e552974f8c374ae44b01
abderahmane-ai <abderahmane.ainouche.ai@gmail.com>
2026-06-12 21:37:46 +0100

    Initial Commit

---
+++ skmetal_core/skmetal_core.h
@@ -0,0 +1,12 @@
+//
+//  skmetal_core.h
+//  skmetal_core
+//
+//  Created by Abderahmane Ainouche on 12/06/2026.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface skmetal_core : NSObject
+
+@end
---
+++ skmetal_core/skmetal_core.m
@@ -0,0 +1,12 @@
+//
+//  skmetal_core.m
+//  skmetal_core
+//
+//  Created by Abderahmane Ainouche on 12/06/2026.
+//
+
+#import "skmetal_core.h"
+
+@implementation skmetal_core
+
+@end
